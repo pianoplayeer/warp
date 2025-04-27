5868082d4d61e6b8722f3071b4191b1297db4dbb
juxiangyu <juxiangyu@xiaohongshu.com>
2025-04-27 12:03:52 +0800

    init

---
+++ src/main/java/com/jxy/warp/WarpApplication.java
@@ -0,0 +1,13 @@
+package com.jxy.warp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jxy/warp/WarpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxy.warp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
